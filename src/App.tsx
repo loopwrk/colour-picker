2412a62e5c98761220ef1da29a660a9d9340db6a
Am4nzi <45455994+Am4nzi@users.noreply.github.com>
2026-05-16 21:07:22 +0100

    install-flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-function App() {
+import { Button } from 'flowbite-react';
 
+function App() {
   return (
-    <>
-      <div className="min-h-screen bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-slate-100 flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Tailwind is alive</h1>
-      </div>
-    </>
-  )
+    <div className="min-h-screen bg-slate-100 dark:bg-slate-900 flex items-center justify-center">
+      <Button>Flowbite is alive</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
