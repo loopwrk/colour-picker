fa6b0263197f65d29ea9f1790963fba0ace1a85c
Am4nzi <45455994+Am4nzi@users.noreply.github.com>
2026-05-16 21:42:47 +0100

    install i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from 'flowbite-react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="min-h-screen bg-slate-100 dark:bg-slate-900 flex items-center justify-center">
-      <Button>Flowbite is alive</Button>
+      <Button>{t('app.smoke')}</Button>
     </div>
   );
 }
